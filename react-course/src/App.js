76935c358933eebdc1b8b806849641015a8d0df9
eotkds <eotkds@gmail.com>
2023-08-17 23:30:44 +0900

    tailwindcss 추가

--- react-course/src/App.js
+++ react-course/src/App.js
@@ -1,7 +1,15 @@
 
 
 function App() {
-  return <div>Hello!</div>
+  return <div className="m-12 bg-[#dfdfdf] font-sans">
+    <h1 className="text-[#333333]  ">My Todos</h1>
+    <div>
+      <h2>TITLE</h2>
+      <div>
+        <button className="font-inherit px-6 py-2 cursor-pointer rounded-[4px] bg-[#800040] text-white m-0 mx-4 border-solid border border-[#800040] hover:border-[#9c1458] hover:bg-[#9c1458]">Delete</button>
+      </div>
+    </div>
+  </div>;
 }
 
 export default App;
