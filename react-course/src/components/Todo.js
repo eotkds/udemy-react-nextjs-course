0fcea19a9f2421595268436b7b448995d22250e8
eotkds <eotkds@gmail.com>
2023-09-05 23:26:58 +0900

    useState 활용

--- react-course/src/components/Todo.js
+++ react-course/src/components/Todo.js
@@ -1,7 +1,15 @@
+import { useState } from 'react';
+
+import Modal from './Modal';
+import Backdrop from './Backdrop';
+
 function Todo(props) {
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+
+
     function deleteHandler() {
-        console.log("Clicked!");
-        console.log(props.text);
+        setModalIsOpen(true);
+
     }
 
     return (
@@ -10,6 +18,8 @@ function Todo(props) {
             <div className="text-right">
                 <button className="font-inherit px-6 py-2 cursor-pointer rounded-[4px] bg-[#800040] text-white m-0 mx-4 border-solid border border-[#800040] hover:border-[#9c1458] hover:bg-[#9c1458]" onClick={deleteHandler}>Delete</button>
             </div>
+            {modalIsOpen && <Modal />}
+            {modalIsOpen && <Backdrop />}
         </div>
     );
 }
