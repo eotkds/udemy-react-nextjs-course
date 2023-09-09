528264bb114711b99901e0062553ffc1a3c7ccc2
eotkds <eotkds@gmail.com>
2023-09-09 21:40:43 +0900

    react router dom 예제 프로젝트 생성 후 pages 적용

---
+++ nextjs-course-code/src/App.js
@@ -0,0 +1,26 @@
+import { Route, Switch } from 'react-router-dom';
+
+import AllMeetupsPage from './pages/AllMeetups';
+import NewMeetupPage from './pages/NewMeetup';
+import FavoritesPage from './pages/Favorites';
+
+
+function App() {
+  return (
+    <div>
+      <Switch>
+        <Route path='/' exact={true}>
+          <AllMeetupsPage />
+        </Route>
+        <Route path='/new-meetup'>
+          <NewMeetupPage />
+        </Route>
+        <Route path='/favorites'>
+          <FavoritesPage />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
