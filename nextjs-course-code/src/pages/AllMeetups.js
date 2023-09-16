e79432f3bfe086d8fa75af60eee352bea3be508e
eotkds <eotkds@gmail.com>
2023-09-16 13:26:57 +0900

    컴포넌트를 통해 데이터 컴포넌트 출력

--- nextjs-course-code/src/pages/AllMeetups.js
+++ nextjs-course-code/src/pages/AllMeetups.js
@@ -1,12 +1,11 @@
+import MeetupList from "../components/meetups/MeetupList.js";
 import DUMMY_DATA from "../data/dummy-data.js";
 
 function AllMeetupsPage() {
     return <section>
         <h1>All Meetups</h1>
         <ul>
-            {DUMMY_DATA.map((meetup) => {
-                return <li key={meetup.id}>{meetup.title}</li>;
-            })}
+            <MeetupList meetups={DUMMY_DATA} />
         </ul>
     </section>;
 }
