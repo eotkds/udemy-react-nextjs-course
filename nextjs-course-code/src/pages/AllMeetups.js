4b588951a83429a405c5b691dae41cf079eed708
eotkds <eotkds@gmail.com>
2023-09-28 21:37:30 +0900

    useEffect 활용으로 데이터 fetching

--- nextjs-course-code/src/pages/AllMeetups.js
+++ nextjs-course-code/src/pages/AllMeetups.js
@@ -1,11 +1,39 @@
 import MeetupList from "../components/meetups/MeetupList.js";
-import DUMMY_DATA from "../data/dummy-data.js";
+import { useState, useEffect } from "react";
+// import DUMMY_DATA from "../data/dummy-data.js";
 
 function AllMeetupsPage() {
+    const [isLoading, setIsLoading] = useState(true);
+    const [loadedMeetups, setLoadedMeetups] = useState([]);
+    useEffect(() => {
+        setIsLoading(true);
+        fetch('https://react-getting-started-760da-default-rtdb.firebaseio.com/meetups.json')
+            .then(response => {
+                console.log(response);
+                return response.json();
+            }).then(data => {
+                const meetups = [];
+                for (const key in data) {
+                    const meetup = {
+                        id: key,
+                        ...data[key]
+                    };
+                    meetups.push(meetup);
+                };
+                setIsLoading(false);
+                setLoadedMeetups(meetups);
+            });
+    }, []);
+
+    if (isLoading) {
+        return <section>
+            <p>Loading...</p>
+        </section>
+    }
     return <section>
         <h1>All Meetups</h1>
         <ul>
-            <MeetupList meetups={DUMMY_DATA} />
+            <MeetupList meetups={loadedMeetups} />
         </ul>
     </section>;
 }
