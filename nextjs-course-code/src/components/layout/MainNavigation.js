e1b29284e41b80e6bb2485c81fb1440b7c995474
eotkds <eotkds@gmail.com>
2023-09-30 23:12:00 +0900

    더 많은 콘텍스트 사용

--- nextjs-course-code/src/components/layout/MainNavigation.js
+++ nextjs-course-code/src/components/layout/MainNavigation.js
@@ -1,7 +1,11 @@
+import { useContext } from 'react';
 import { Link } from 'react-router-dom'
 import classes from './MainNavigation.module.css';
+import FavoritesContext from '../../store/favorite-context';
 
 function MainNavigation() {
+    const favoritesCtx = useContext(FavoritesContext);
+
     return (
         <header className={classes.header}>
             <div className={classes.logo}>React Meetup</div>
@@ -14,7 +18,10 @@ function MainNavigation() {
                         <Link to='/new-meetup'>Add New Meetup</Link>
                     </li>
                     <li>
-                        <Link to='/favorites'>My Favorites</Link>
+                        <Link to='/favorites'>
+                            <span className={classes.badge}>{favoritesCtx.totalFavorites}</span>
+                            My Favorites
+                        </Link>
                     </li>
                 </ul>
             </nav>
