47510c74a6bc2ac17bf87bfb9caefaf65ebbbd60
eotkds <eotkds@gmail.com>
2023-10-22 00:13:15 +0900

    ISR 적용

--- pre-render-course/pages/index.js
+++ pre-render-course/pages/index.js
@@ -14,6 +14,7 @@ function HomePage(props) {
 }
 
 export async function getStaticProps() {
+    console.log("(Re-)Generating...");
     const filepath = path.join(process.cwd(), "data", "dummy-backend.json");
     const jsonData = await fs.readFile(filepath);
     const data = JSON.parse(jsonData);
@@ -22,6 +23,7 @@ export async function getStaticProps() {
         props: {
             products: data.products,
         },
+        revalidate: 10,
     };
 }
 
