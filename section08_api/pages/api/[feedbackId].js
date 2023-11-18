374ad3f0badc0aecd0d178d644957a0d415da652
eotkds <eotkds@gmail.com>
2023-11-19 01:49:36 +0900

    section08 : 동적 API 라우스 생성

---
+++ section08_api/pages/api/[feedbackId].js
@@ -0,0 +1,16 @@
+import { buildFeedbackPath, extractFeedback } from './feedback';
+
+function handler(req, res) {
+    // console.log(req);
+    if (req.method === 'GET') {
+        const feedbackId = req.query.feedbackId;
+        const filepath = buildFeedbackPath();
+        const feedbackData = extractFeedback(filepath);
+
+        const selectedFeedback = feedbackData.find((feedback) => feedback.id === feedbackId);
+
+        res.status(200).json({ message: 'This works!', feedback: selectedFeedback });
+    }
+}
+
+export default handler;
--- section08_api/pages/feedback/index.js
+++ section08_api/pages/feedback/index.js
@@ -1,13 +1,31 @@
+import { useState, Fragment } from 'react';
 import { buildFeedbackPath, extractFeedback } from '../api/feedback';
 
 function FeedbackPage(props) {
+    const [feedbackData, setFeedbackData] = useState();
+
+    function loadFeedbackHandler(id) {
+        fetch(`/api/${id}`)
+            .then((response) => response.json())
+            .then((data) => {
+                setFeedbackData(data.feedback);
+                console.log(data);
+            });
+    }
+
     const { feedbackItems } = props;
     return (
-        <ul>
-            {feedbackItems.map((item) => (
-                <li key={item.id}>{item.text}</li>
-            ))}
-        </ul>
+        <Fragment>
+            {feedbackData && <p>{feedbackData.email}</p>}
+            <ul>
+                {feedbackItems.map((item) => (
+                    <li key={item.id}>
+                        {item.text}
+                        <button onClick={loadFeedbackHandler.bind(null, item.id)}>Show Detail</button>
+                    </li>
+                ))}
+            </ul>
+        </Fragment>
     );
 }
 
