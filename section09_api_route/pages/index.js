3e9a3a1a38728137c6b12e17b8bb4c2df70a5104
eotkds <eotkds@gmail.com>
2023-11-21 21:29:43 +0900

    section09 : create project

---
+++ section09_api_route/pages/index.js
@@ -0,0 +1,21 @@
+import Head from 'next/head';
+
+import { getFeaturedEvents } from '../dummy-data';
+import EventList from '../components/events/event-list';
+import NewsletterRegistration from '../components/input/newsletter-registration';
+
+function HomePage() {
+    const featuredEvents = getFeaturedEvents();
+    return (
+        <div>
+            <Head>
+                <title>NextJS Events</title>
+                <meta name="description" content="Find a lot of great events that allow you to evolve..." />
+            </Head>
+            <NewsletterRegistration />
+            <EventList items={featuredEvents} />
+        </div>
+    );
+}
+
+export default HomePage;
