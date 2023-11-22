0c9df40c83fc94878e9363b1abc9ab5340a3c94a
eotkds <eotkds@gmail.com>
2023-11-22 23:12:53 +0900

    section09 : 뉴스레터 등록 버튼 이벤트, 등록 api

---
+++ section09_api_route/pages/api/newsletter.js
@@ -0,0 +1,13 @@
+function handler(req, res) {
+    if (req.method == 'POST') {
+        const userEmail = req.body.email;
+
+        if (!userEmail || !userEmail.includes('@')) {
+            res.status(422).json({ message: 'Invalid email address.' });
+            return;
+        }
+        res.status(201).json({ message: 'Signed up!', email: userEmail });
+    }
+}
+
+export default handler;
