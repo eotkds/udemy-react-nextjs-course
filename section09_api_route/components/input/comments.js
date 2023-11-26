d4ffd5ed25b18b8ec140f7002fb0dd596b788068
eotkds <eotkds@gmail.com>
2023-11-26 11:58:34 +0900

    section09 comment list

--- section09_api_route/components/input/comments.js
+++ section09_api_route/components/input/comments.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import CommentList from './comment-list';
 import NewComment from './new-comment';
@@ -8,9 +8,29 @@ function Comments(props) {
     const { eventId } = props;
 
     const [showComments, setShowComments] = useState(false);
+    const [comments, setComments] = useState([]);
+    useEffect(() => {
+        if (showComments) {
+            fetch('/api/comments/' + eventId)
+                .then((response) => response.json())
+                .then((data) => {
+                    // console.log(data);
+                    setComments(data.comments);
+                });
+        }
+    }, [showComments]);
 
     function toggleCommentsHandler() {
+        console.log(showComments);
         setShowComments((prevStatus) => !prevStatus);
+        // if (!showComments) {
+        //     fetch('/api/comments/' + eventId)
+        //         .then((response) => response.json())
+        //         .then((data) => {
+        //             console.log(data);
+        //             setComments(data.comments);
+        //         });
+        // }
     }
 
     function addCommentHandler(commentData) {
@@ -30,7 +50,7 @@ function Comments(props) {
         <section className={classes.comments}>
             <button onClick={toggleCommentsHandler}>{showComments ? 'Hide' : 'Show'} Comments</button>
             {showComments && <NewComment onAddComment={addCommentHandler} />}
-            {showComments && <CommentList />}
+            {showComments && <CommentList items={comments} />}
         </section>
     );
 }
