f0213d5b6b212e847e7a87d1a117bb258704f36e
eotkds <eotkds@gmail.com>
2023-11-26 14:39:27 +0900

    section09 MongoDB 연결; 뉴스레터 api에 insert 적용; 환경변수 설정(env)

--- section09_api_route/pages/api/newsletter.js
+++ section09_api_route/pages/api/newsletter.js
@@ -1,4 +1,10 @@
-function handler(req, res) {
+import { MongoClient } from 'mongodb';
+
+async function handler(req, res) {
+    const url = `${process.env.DB_HOST}`;
+    const client = new MongoClient(url);
+    const dbName = 'newsletter';
+
     if (req.method == 'POST') {
         const userEmail = req.body.email;
 
@@ -6,6 +12,13 @@ function handler(req, res) {
             res.status(422).json({ message: 'Invalid email address.' });
             return;
         }
+
+        await client.connect();
+        const db = client.db(dbName);
+        const collection = db.collection('emails');
+        const insertResult = await collection.insertOne({ email: userEmail });
+
+        client.close();
         res.status(201).json({ message: 'Signed up!', email: userEmail });
     }
 }
