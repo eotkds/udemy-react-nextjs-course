9df0bf43ae89d2608b50b479274c08a001880ccf
eotkds <eotkds@gmail.com>
2023-12-02 21:51:59 +0900

    section10 : Notification 테스트 적용, context 초기 생성

--- section09_api_route/pages/_app.js
+++ section09_api_route/pages/_app.js
@@ -1,17 +1,22 @@
 import Head from 'next/head';
 import Layout from '../components/layout/layout';
 import '../styles/globals.css';
+import Notification from '../components/ui/notification';
+import { NotificationContextProvider } from '../store/notification-context';
 
 function MyApp({ Component, pageProps }) {
     return (
-        <Layout>
-            <Head>
-                <title>NextJS Events</title>
-                <meta name="description" content="NextJS Events" />
-                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            </Head>
-            <Component {...pageProps} />
-        </Layout>
+        <NotificationContextProvider>
+            <Layout>
+                <Head>
+                    <title>NextJS Events</title>
+                    <meta name="description" content="NextJS Events" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                </Head>
+                <Component {...pageProps} />
+                <Notification title="test" message="test message." status="error" />
+            </Layout>
+        </NotificationContextProvider>
     );
 }
 
