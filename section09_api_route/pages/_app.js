75974c41d8676c8e694a054274030cdb108a3d4f
eotkds <eotkds@gmail.com>
2023-12-05 21:46:50 +0900

    section10 : 컴포넌트 내에서 콘텍스트 데이터 사용 & 뉴스레터 알림 사용

--- section09_api_route/pages/_app.js
+++ section09_api_route/pages/_app.js
@@ -1,7 +1,6 @@
 import Head from 'next/head';
 import Layout from '../components/layout/layout';
 import '../styles/globals.css';
-import Notification from '../components/ui/notification';
 import { NotificationContextProvider } from '../store/notification-context';
 
 function MyApp({ Component, pageProps }) {
@@ -14,7 +13,6 @@ function MyApp({ Component, pageProps }) {
                     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 </Head>
                 <Component {...pageProps} />
-                <Notification title="test" message="test message." status="error" />
             </Layout>
         </NotificationContextProvider>
     );
