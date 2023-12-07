a1a0a33f58d2ab28c7389fd0bc69e3995869cc60
eotkds <eotkds@gmail.com>
2023-12-07 22:05:36 +0900

    section10 : 뉴스레터, 댓글 알람 적용, 알람클릭시 제거, 자동 제거 적용

--- section09_api_route/components/input/comments.js
+++ section09_api_route/components/input/comments.js
@@ -1,21 +1,25 @@
-import { useEffect, useState } from 'react';
-
+import { useEffect, useState, useContext } from 'react';
 import CommentList from './comment-list';
 import NewComment from './new-comment';
 import classes from './comments.module.css';
+import NotificationContext from '../../store/notification-context';
 
 function Comments(props) {
     const { eventId } = props;
 
     const [showComments, setShowComments] = useState(false);
     const [comments, setComments] = useState([]);
+    const notificationCtx = useContext(NotificationContext);
+    const [isFetchingComments, setIsFetchingComments] = useState(false);
     useEffect(() => {
+        setIsFetchingComments(true);
         if (showComments) {
             fetch('/api/comments/' + eventId)
                 .then((response) => response.json())
                 .then((data) => {
-                    // console.log(data);
+                    // console.log(data);\
                     setComments(data.comments);
+                    setIsFetchingComments(false);
                 });
         }
     }, [showComments]);
@@ -35,6 +39,13 @@ function Comments(props) {
 
     function addCommentHandler(commentData) {
         // send data to API
+
+        notificationCtx.showNotification({
+            title: 'fetching comments',
+            message: 'fetching comments for ' + eventId,
+            status: 'pending',
+        });
+
         fetch('/api/comments/' + eventId, {
             method: 'POST',
             body: JSON.stringify(commentData),
@@ -42,15 +53,34 @@ function Comments(props) {
                 'Content-Type': 'application/json',
             },
         })
-            .then((response) => response.json())
-            .then((data) => console.log(data));
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error('Something went wrong!');
+                }
+                return response.json();
+            })
+            .then((data) => {
+                notificationCtx.showNotification({
+                    title: 'fetching Success!',
+                    message: 'Successfully fetched comments!',
+                    status: 'success',
+                });
+            })
+            .catch((error) => {
+                notificationCtx.showNotification({
+                    title: 'Error!',
+                    message: error.message || 'Something went wrong!',
+                    status: 'error',
+                });
+            });
     }
 
     return (
         <section className={classes.comments}>
             <button onClick={toggleCommentsHandler}>{showComments ? 'Hide' : 'Show'} Comments</button>
             {showComments && <NewComment onAddComment={addCommentHandler} />}
-            {showComments && <CommentList items={comments} />}
+            {showComments && !isFetchingComments && <CommentList items={comments} />}
+            {showComments && isFetchingComments && <p>Loading...</p>}
         </section>
     );
 }
