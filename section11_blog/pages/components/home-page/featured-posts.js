9263f6fdb1694bbaa32490819acc00d2728dc2a1
eotkds <eotkds@gmail.com>
2023-12-12 22:07:53 +0900

    section11 : post section 생성

---
+++ section11_blog/pages/components/home-page/featured-posts.js
@@ -0,0 +1,11 @@
+import classes from "./featured-posts.module.css";
+
+function FeaturedPosts() {
+    return (
+        <section className={classes.latest}>
+            <h2>Featured Posts</h2>A list of featured posts
+        </section>
+    );
+}
+
+export default FeaturedPosts;
