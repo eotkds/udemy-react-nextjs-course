6ee7625c977a8d8aba677cc79b4a71152e1c11ef
eotkds <eotkds@gmail.com>
2023-12-13 23:42:34 +0900

    section11 : 게시물 더미데이터를 통해 렌더링

--- section11_blog/pages/components/posts/post-item.js
+++ section11_blog/pages/components/posts/post-item.js
@@ -12,10 +12,10 @@ function PostItem(props) {
     });
 
     const imagePath = `/images/posts/${slug}/${image}`;
-
+    const linkPath = `/posts/${slug}`;
     return (
         <li className={classes.post}>
-            <Link>
+            <Link href={linkPath}>
                 <div className={classes.image}>
                     <Image src={imagePath} alt={title} width={300} height={200} />
                 </div>
--- section11_blog/pages/components/posts/posts-grid.js
+++ section11_blog/pages/components/posts/posts-grid.js
@@ -3,10 +3,11 @@ import classes from "./posts-grid.module.css";
 
 function PostsGrid(props) {
     const { posts } = props;
+
     return (
         <ul className={classes.grid}>
             {posts.map((post) => (
-                <PostItem />
+                <PostItem key={post.slug} post={post} />
             ))}
         </ul>
     );
