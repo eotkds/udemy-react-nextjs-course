4e59d3270b9c9c0a82dc118e9aea178664715056
eotkds <eotkds@gmail.com>
2023-12-18 22:44:29 +0900

    section11 : PostDetail 페이지 생성

--- section11_blog/pages/posts/[slug].js
+++ section11_blog/pages/posts/[slug].js
@@ -1,2 +1,6 @@
-function PostsDetailPage() {}
+import PostContent from "../components/posts/post-detail/post-content";
+
+function PostsDetailPage() {
+    return <PostContent />;
+}
 export default PostsDetailPage;
