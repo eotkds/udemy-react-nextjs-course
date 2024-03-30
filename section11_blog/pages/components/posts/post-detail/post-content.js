c138d2c8fdbd7a43296d05c06d3222b50ab088cb
eotkds <eotkds@gmail.com>
2024-03-30 18:18:41 +0900

    section11 - 마크다운에서 component를 활용하여 이미지 랜더링

--- section11_blog/pages/components/posts/post-detail/post-content.js
+++ section11_blog/pages/components/posts/post-detail/post-content.js
@@ -1,15 +1,44 @@
 import Markdown from "react-markdown";
 import PostHeader from "./post-header";
 import classes from "./post-content.module.css";
+import Image from "next/image";
 
 function PostContent(props) {
     const { post } = props;
     const imagePath = `/images/posts/${post.slug}/${post.image}`;
+    const customRenderers = {
+        // 이 부분이 정확한 차이가 이해가 안됨
+
+        // img(image) {
+        //     return <Image src={`/images/posts/${post.slug}/${image.src}`} alt={image.alt} width={600} height={300} />;
+        // },
+        p(paragraph) {
+            // console.log(paragraph);
+            const { node } = paragraph;
+
+            if (node.children[0].tagName === "img") {
+                const image = node.children[0];
+                return (
+                    <div className={classes.image}>
+                        <Image
+                            src={`/images/posts/${post.slug}/${image.properties.src}`}
+                            alt={image.properties.alt}
+                            width={600}
+                            height={300}
+                            priority
+                        />
+                    </div>
+                );
+            }
+
+            return <p>{paragraph.children}</p>;
+        },
+    };
 
     return (
         <article className={classes.content}>
             <PostHeader title={post.title} image={imagePath} />
-            <Markdown>{post.content}</Markdown>
+            <Markdown components={customRenderers}>{post.content}</Markdown>
         </article>
     );
 }
