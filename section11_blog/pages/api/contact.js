06e05bd14ccbf2ee6efabda8a23076e29bfa2152
eotkds <eotkds@gmail.com>
2024-04-05 22:17:55 +0900

    section11 - 알림 추가; context 생성하지는 않았지만

--- section11_blog/pages/api/contact.js
+++ section11_blog/pages/api/contact.js
@@ -3,7 +3,6 @@ import { MongoClient, ServerApiVersion } from "mongodb";
 async function handler(req, res) {
     // console.log(res);
     const uri = process.env.MONGODB_URI;
-    const client = new MongoClient(uri);
 
     if (req.method === "POST") {
         const { email, name, message } = req.body;
@@ -20,16 +19,15 @@ async function handler(req, res) {
         };
 
         try {
+            const client = new MongoClient(uri);
             const database = client.db("my-site");
             const myMessage = database.collection("messages");
             const result = await myMessage.insertOne(newMessage);
             newMessage.id = result.insertedId;
             console.log(`A document was inserted with the _id: ${result.insertedId}`);
-        } catch (error) {
-            console.log(error);
-            res.status(500).json({ message: "Could not store message" });
-        } finally {
             await client.close();
+        } catch (error) {
+            res.status(500).json({ message: "Could not store message" });
         }
 
         res.status(201).json({ message: "Successfully stored message!", message: newMessage });
