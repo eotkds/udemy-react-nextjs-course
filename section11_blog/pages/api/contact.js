9f59edf622126c70ae5e5d24b6b1b8723e9c4177
eotkds <eotkds@gmail.com>
2024-04-10 00:03:56 +0900

    Section11 - env 적용

--- section11_blog/pages/api/contact.js
+++ section11_blog/pages/api/contact.js
@@ -1,6 +1,7 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
 
 async function handler(req, res) {
+    const connectionString = `mongodb+srv://${process.env.mongodb_username}:${process.env.mongodb_password}@${process.env.mongodb_cluster_name}.vrhmvul.mongodb.net/?retryWrites=true&w=majority&appName=${process.env.mongodb_cluster_name}`;
     const uri = process.env.MONGODB_URI;
 
     if (req.method === "POST") {
@@ -18,12 +19,11 @@ async function handler(req, res) {
         };
 
         try {
-            const client = new MongoClient(uri);
-            const database = client.db("my-site");
+            const client = new MongoClient(connectionString);
+            const database = client.db(process.env.mongodb_database);
             const myMessage = database.collection("messages");
             const result = await myMessage.insertOne(newMessage);
             newMessage.id = result.insertedId;
-            console.log(`A document was inserted with the _id: ${result.insertedId}`);
             await client.close();
         } catch (error) {
             res.status(500).json({ message: "Could not store message" });
