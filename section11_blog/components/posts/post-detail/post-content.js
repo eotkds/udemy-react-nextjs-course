ac57b4ea560b8dec174138946394a2dddbc4e856
eotkds <eotkds@gmail.com>
2024-04-14 00:29:12 +0900

    Section11 - code 크기 줄이기

--- section11_blog/components/posts/post-detail/post-content.js
+++ section11_blog/components/posts/post-detail/post-content.js
@@ -2,8 +2,12 @@ import Markdown from "react-markdown";
 import PostHeader from "./post-header";
 import classes from "./post-content.module.css";
 import Image from "next/image";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
+
+import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
+import atomDark from "react-syntax-highlighter/dist/cjs/styles/prism/atom-dark";
+import js from "react-syntax-highlighter/dist/cjs/languages/prism/javascript";
+
+SyntaxHighlighter.registerLanguage("javascript", js);
 
 function PostContent(props) {
     const { post } = props;
